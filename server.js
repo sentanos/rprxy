442ab083e27c0269f256f288cc50ecb7aa729f47
FxllenCode <eswebber07@gmail.com>
2021-08-04 11:41:04 -0400

    updated user-agent header to follow new standard

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ function onProxyError (err, req, res) {
 }
 
 function onProxyReq (proxyReq, req, res, options) {
-  proxyReq.setHeader('User-Agent', 'Mozilla');
+  proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36');
   proxyReq.removeHeader('roblox-id');
 }
 
